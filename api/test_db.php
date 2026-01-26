8bd726eeb1af1ff9a2ddaf439066b584d6787761
originslone-sys <originslone@gmail.com>
2026-01-26 14:36:50 -0300

    Update test_db.php

--- api/test_db.php
+++ api/test_db.php
@@ -1,10 +1,13 @@
 <?php
 require_once "config.php";
 
-$db = getDatabaseConnection();
-
-if ($db) {
-    echo "Conectado ao banco com sucesso!";
-} else {
-    echo "Erro ao conectar no banco.";
+function getDatabaseConnection() {
+    try {
+        $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+        return new PDO($dsn, DB_USER, DB_PASS, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    } catch(PDOException $e) {
+        die("Erro PDO: " . $e->getMessage());
+    }
 }
