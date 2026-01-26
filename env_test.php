fb62200f134d746e43d1dfa6ddf3e5d0dae29b81
originslone-sys <originslone@gmail.com>
2026-01-26 14:20:06 -0300

    Create env_test.php

---
+++ env_test.php
@@ -0,0 +1,8 @@
+<?php
+echo "<pre>";
+var_dump(getenv("MYSQLHOST"));
+var_dump(getenv("MYSQLUSER"));
+var_dump(getenv("MYSQLPASSWORD"));
+var_dump(getenv("MYSQLDATABASE"));
+var_dump(getenv("MYSQLPORT"));
+echo "</pre>";
