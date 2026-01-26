f2574c3f75cd4099698051b12b9499a1871928ba
originslone-sys <originslone@gmail.com>
2026-01-26 15:53:27 -0300

    Delete test_tables.php

--- test_tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/api/config.php";
-
-$db = getDatabaseConnection();
-$stmt = $db->query("SHOW TABLES");
-
-echo "<pre>";
-print_r($stmt->fetchAll(PDO::FETCH_COLUMN));
-echo "</pre>";
