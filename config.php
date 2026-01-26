f3598d32c7ce7dbe13c2d9838f7df21e423ee001
originslone-sys <originslone@gmail.com>
2026-01-26 16:45:20 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,3 @@
 <?php
-// config.php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'aster489_car2');
-define('DB_PASS', 'opeH#VSA7&_q+LzL');
-define('DB_NAME', 'aster489_car1');
-?>
+// config.php (raiz) - ponte para o config oficial do sistema
+require_once __DIR__ . "/api/config.php";
