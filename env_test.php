57c47c416d5a8a5b575cade9766267a4b74aee1f
originslone-sys <originslone@gmail.com>
2026-01-26 14:45:10 -0300

    Delete env_test.php

--- env_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<pre>";
-var_dump(getenv("MYSQLHOST"));
-var_dump(getenv("MYSQLUSER"));
-var_dump(getenv("MYSQLPASSWORD"));
-var_dump(getenv("MYSQLDATABASE"));
-var_dump(getenv("MYSQLPORT"));
-echo "</pre>";
