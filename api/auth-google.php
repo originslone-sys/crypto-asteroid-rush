f00ca7d1373aa205043b0a5443d341714667a2d9
originslone-sys <originslone@gmail.com>
2026-01-29 23:55:05 -0300

    Update auth-google.php

--- api/auth-google.php
+++ api/auth-google.php
@@ -10,6 +10,13 @@ require_once __DIR__ . "/config.php";
 setCORSHeaders();
 header('Content-Type: application/json; charset=utf-8');
 
+$raw = file_get_contents('php://input');
+$input = json_decode($raw, true);
+
+if (is_array($input)) {
+    $_POST = array_merge($_POST, $input);
+}
+
 $input = getRequestInput();
 $action = $input['action'] ?? 'verify';
 
