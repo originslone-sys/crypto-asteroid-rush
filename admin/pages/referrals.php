55df17b96c9da07b3ea32f28f85508b17fcdf6d1
originslone-sys <originslone@gmail.com>
2026-02-06 13:42:32 -0300

    Update referrals.php

--- admin/pages/referrals.php
+++ admin/pages/referrals.php
@@ -30,7 +30,7 @@ try {
     $error = $e->getMessage();
 }
 
-function formatBRL($v) { return 'R$ ' . number_format($v ?? 0, 2, ',', '.'); }
+// formatBRL() já definida em config.php
 ?>
 
 <div class="main-content">
