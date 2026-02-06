b6e180c5afa69f16c24cc46e131bcfb21fbc4188
originslone-sys <originslone@gmail.com>
2026-02-06 13:41:32 -0300

    Update transactions.php

--- admin/pages/transactions.php
+++ admin/pages/transactions.php
@@ -39,7 +39,7 @@ try {
     $error = $e->getMessage();
 }
 
-function formatBRL($v) { return 'R$ ' . number_format($v ?? 0, 2, ',', '.'); }
+// formatBRL() já definida em config.php
 
 $typeLabels = [
     'game_earning' => ['Ganho Jogo', 'success'],
